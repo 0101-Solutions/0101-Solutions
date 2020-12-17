8d82e0a33fc5d8b13984a7c9c800f165def5bd57
Toxic254 <cyrus_g@outlook.com>
2020-12-17 13:53:37 +0300

    Added headers broke the file. Think it works on APIs lemme explore.

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.type('application/json')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
